ad69b85a81db456d51c8b45244927fd86d7b53bc
Samuel Sumilat <51332843+samuelsumilat@users.noreply.github.com>
2023-10-20 07:51:47 +0800

    Update guestbook.js

--- js/guestbook.js
+++ js/guestbook.js
@@ -1,13 +1,16 @@
-document.getElementById("guestbook-form").addEventListener("submit", function(e) {
+document.getElementById("guestbookForm").onsubmit = function(e) {
   e.preventDefault();
-
-  const nama = document.getElementById("nama").value;
-  const ucapan = document.getElementById("ucapan").value;
-  const kehadiran = document.getElementById("kehadiran").value;
-  
-  google.script.run.insertData(nama, ucapan, kehadiran);
-
-  document.getElementById("nama").value = "";
-  document.getElementById("ucapan").value = "";
-  document.getElementById("kehadiran").value = "ada";
-});
+  var data = new FormData(this);
+  fetch('https://script.google.com/macros/s/AKfycbzf9sScQFfSS3X2NSdEoN3zObMKRvF8-rC3-W7uW-mEgB1-Z27M/exec', {
+    method: 'POST',
+    body: data
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.result === 'success') {
+      alert('Data berhasil disimpan.');
+    } else {
+      alert('Gagal menyimpan data.');
+    }
+  });
+}
