012e65fb486fca1ebe8ef85bdd9c729ba006e625
Samuel Sumilat <51332843+samuelsumilat@users.noreply.github.com>
2023-10-19 18:14:34 +0800

    Update guestbook.js

--- js/guestbook.js
+++ js/guestbook.js
@@ -1,28 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var form = document.getElementById("guestBookForm");
-    var guestList = document.getElementById("guestList");
+document.getElementById("guestbook-form").addEventListener("submit", function(e) {
+  e.preventDefault();
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+  const nama = document.getElementById("nama").value;
+  const ucapan = document.getElementById("ucapan").value;
+  const kehadiran = document.getElementById("kehadiran").value;
+  
+  google.script.run.insertData(nama, ucapan, kehadiran);
 
-        var name = document.getElementById("name").value;
-        var message = document.getElementById("message").value;
-        var attendance = document.getElementById("attendance").value;
-
-        // Kirim data ke Google Apps Script untuk disimpan di spreadsheet
-        google.script.run.addGuestEntry(name, message, attendance);
-
-        // Perbarui daftar buku tamu
-        updateGuestList();
-    });
-
-    function updateGuestList() {
-        // Ambil data buku tamu dari Google Sheets
-        google.script.run.withSuccessHandler(function (guestbookEntries) {
-            guestList.innerHTML = "<h2>Daftar Buku Tamu</h2>" + guestbookEntries.join("<br>");
-        }).getGuestEntries();
-    }
-
-    // Perbarui daftar buku tamu saat halaman dimuat
-    updateGuestList();
+  document.getElementById("nama").value = "";
+  document.getElementById("ucapan").value = "";
+  document.getElementById("kehadiran").value = "ada";
 });
